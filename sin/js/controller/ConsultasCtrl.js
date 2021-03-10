1485de6395cd8a7698bd01e9eb303a05d9c92c5d
Rai Lima de Oliveira <rai.oliveira@saude.ce.gov.br>
2021-03-10 14:38:54 -0300

    Conclui tela de consultas

--- sin/js/controller/ConsultasCtrl.js
+++ sin/js/controller/ConsultasCtrl.js
@@ -1,51 +1,259 @@
-
-inicio_mod.controller('ConsultasCtrl', function ($scope) {
+inicio_mod.controller('ConsultasCtrl', function ($scope, $http, $filter) {
   $scope.subtitulo = "Gerencie todas as consultas";
 
+  this.listarConsultas = listarConsultas;
+  this.montarTabelaConsultas = montarTabelaConsultas;
+  this.formatarStatus = formatarStatus;
+  this.formatarData = formatarData;
+
   $scope.init = function () {
-    var dados = [{
-      nome: "Maria Joaquina 1 ",
-      data: "01/01/2020",
-      status: "Agendada",
-      urgencia: 10,
-    }, {
-      nome: "João das Couves",
-      data: "10/01/2020",
-      status: "Confirmada",
-      urgencia: 10,
-    }, {
-      nome: "José",
-      data: "02/02/2020",
-      status: "Encerrada",
-      urgencia: 10,
-    }]
-    monta_tabela_consultas(dados);
+    $scope.consulta = {};
+    $scope.consulta.usuario = {};
+    $scope.psf_id = 1;
+    carregarUsuariosDoPsf();
+    listarConsultas();
   }
 
   $scope.init();
-});
 
-function monta_tabela_consultas(dados) {
-  var t = $('#tabela').DataTable({
-    "columns": [
-      { "width": "40%" },
-      { "width": "15%" },
-      { "width": "15%" },
-      { "width": "15%" },],
-    "info": false,
-    "paging": true,
-    "pageLength": 10
-  });
-
-  for (var i = 0; i < dados.length; i++) {
-    t.row.add([
-      dados[i].nome,
-      dados[i].data ? dados[i].data : '-',
-      dados[i].status ? dados[i].status : '-',
-      dados[i].urgencia ? dados[i].urgencia : '-',
-    ]).draw();
+  $scope.abrirModalCadastrar = function () {
+    $scope.consulta.id = undefined;
+    $scope.consulta.usuario = undefined;
+    $scope.consulta.data = undefined;
+    $scope.consulta.status = undefined;
+    $scope.consulta.urgencia = undefined;
+    $scope.consulta.motivo = undefined;
+    
+    $("#modal").modal();
   };
 
-  instancia_click();
+  $scope.salvar = function () {
+    if ($scope.consulta.id) {
+      $scope.editar();
+    } else {
+      $scope.cadastrar();
+    }
+  };
 
-}
+  $scope.abrirModalEditar = function () {
+    var dados = $scope.selectedRow.data();
+    $("#modal").modal();
+
+    $scope.consulta.id = dados[0];
+    $scope.consulta.usuario = dados[1] ? {id: dados[1]} : {};
+    $scope.consulta.usuario_id = dados[1];
+    $scope.consulta.data = moment(new Date(dados[3]));
+    $scope.consulta.status = dados[4].toLowerCase();
+    $scope.consulta.urgencia = dados[5];
+    $scope.consulta.motivo = dados[6];
+  };
+
+  $scope.cadastrar = function () {
+    swal({
+      title: "Cadastrar consulta?",
+      text: "Você tem certeza que deseja cadastrar a consulta?",
+      type: "warning",
+      cancelButtonText: "Cancelar",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Sim!",
+    }).then(
+      function (isConfirm) {
+        $scope.consulta.psf_id = 1;
+        $scope.consulta.usuario_id = $scope.consulta.usuario;
+        if (isConfirm) {
+          $http.post(__env.apiUrl + "/consultas/", $scope.consulta).then(
+            function (response) {
+              $("#tabela")
+                .DataTable()
+                .row.add(dadosDaRow(response.data))
+                .draw();
+              $("#modal").modal("toggle");
+              showConfirmation("Consulta cadastrada com sucesso");
+            },
+            function (error) {
+              swal({
+                title: "Erro ao cadastrar",
+                text: "Houve um erro na tentativa de cadastrar a consulta",
+                type: "error",
+                timer: 2000,
+              }).then(
+                function () {},
+                function () {}
+              );
+            }
+          );
+        }
+      },
+      function () {}
+    );
+  };
+
+  $scope.editar = function () {
+    swal({
+      title: "Atualizar consulta?",
+      text: "Você tem certeza que deseja atualizar a consulta?",
+      type: "warning",
+      cancelButtonText: "Cancelar",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Sim!",
+    }).then(
+      function (isConfirm) {
+        $scope.consulta.psf_id = 1;
+        if (isConfirm) {
+          $http
+            .put(__env.apiUrl + "/consultas/" + $scope.consulta.id, $scope.consulta)
+            .then(
+              function (response) {
+                $scope.selectedRow.data(dadosDaRow(response.data)).draw();
+                $("#modal").modal("toggle");
+                showConfirmation("Consulta atualizada com sucesso");
+              },
+              function (error) {
+                swal({
+                  title: "Erro ao atualizar",
+                  text:
+                    "Houve um erro na tentativa de atualizar a consulta selecionada",
+                  type: "error",
+                  timer: 2000,
+                }).then(
+                  function () {},
+                  function () {}
+                );
+              }
+            );
+        }
+      },
+      function () {}
+    );
+  };
+
+  $scope.excluir = function () {
+    var idConsultaSelecionado = $scope.selectedRow.data()[0];
+    swal({
+      title: "Excluir consulta?",
+      text: "Você tem certeza que deseja excluir a consulta?",
+      type: "warning",
+      cancelButtonText: "Cancelar",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Sim, Excluir!",
+    }).then(
+      function (isConfirm) {
+        if (isConfirm) {
+          $http
+            .delete(__env.apiUrl + "/consultas/" + idConsultaSelecionado)
+            .then(
+              function (response) {
+                $scope.selectedRow.remove().draw();
+                $('#btn_editar').prop('disabled', true);
+                $('#btn_excluir').prop('disabled', true);
+                showConfirmation("Consulta excluída com sucesso");
+              },
+              function (error) {
+                swal({
+                  title: "Erro ao excluir",
+                  text:
+                    "Houve um erro na tentativa de excluir a consulta selecionada",
+                  type: "error",
+                  timer: 2000,
+                }).then(
+                  function () {},
+                  function () {}
+                );
+              }
+            );
+        }
+      },
+      function () {}
+    );
+  };
+
+ function carregarUsuariosDoPsf() {
+    $http.get(__env.apiUrl + `/psfs/${$scope.psf_id}/usuarios`)
+    .then(function(response){
+        $scope.usuarios = response.data;
+      }, function(error){
+        console.log(error.data);
+    });
+  }
+
+  function listarConsultas() {
+    $http.get(__env.apiUrl + `/psfs/${$scope.psf_id}/consultas`).then(
+      function (response) {
+        montarTabelaConsultas(response.data);
+      },
+      function (error) {
+        swal({
+          title: "Erro ao listar",
+          text:
+            "Houve um erro na tentativa de listar as consultas",
+          type: "error",
+          timer: 2000,
+        }).then(
+          function () {},
+          function () {}
+        );
+      }
+    );
+  }
+
+  function montarTabelaConsultas(dados) {
+    var t = $("#tabela").DataTable({
+      columns: [
+        { width: "1%" },
+        { width: "1%" },
+        { width: "40%" },
+        { width: "15%" },
+        { width: "15%" },
+        { width: "13%" },
+        { width: "15%" },
+      ],
+      info: false,
+      paging: true,
+      pageLength: 10,
+    });
+
+    $("#tabela tbody").on("click", "tr", function () {
+      $scope.selectedRow = t.row(this);
+    });
+
+    $('#modal').on('hide.bs.modal', function (e) {
+      $scope.consulta = {};
+      $scope.consulta.usuario = {};
+    });
+
+    for (var i = 0; i < dados.length; i++) {
+      t.row.add(dadosDaRow(dados[i])).draw();
+    }
+
+    t.column(0).visible(false);
+    t.column(1).visible(false);
+    instancia_click();
+  }
+
+  function dadosDaRow(dados) {
+    return [
+      dados.id,
+      dados.usuario_id,
+      dados.usuario ? dados.usuario.nome : "-",
+      dados.data ? formatarData(dados.data) : "-",
+      dados.status ? formatarStatus(dados.status) : "-",
+      dados.urgencia ? dados.urgencia : "-",
+      dados.motivo ? dados.motivo : "-",
+    ];
+  }
+
+  function formatarStatus(status) {
+    return status.toUpperCase();
+  }
+  
+  function formatarData(data) {
+    return $filter('date')(new Date(data), "dd/MM/yyyy", "UTC");
+  }
+
+});
